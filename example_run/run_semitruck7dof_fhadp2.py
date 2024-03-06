a876ad87571f48e92e71fda3f19474ffa4480c40
wwx <17888819590@163.com>
2024-03-06 13:14:23 +0800

    新增瓜卡run代码

---
+++ example_run/run_semitruck7dof_fhadp2.py
@@ -0,0 +1,52 @@
+#  Copyright (c). All Rights Reserved.
+#  General Optimal control Problem Solver (GOPS)
+#  Intelligent Driving Lab (iDLab), Tsinghua University
+#
+#  Creator: iDLab
+#  Lab Leader: Prof. Shengbo Eben Li
+#  Email: lisb04@gmail.com
+#
+#  Description: run a closed-loop system
+
+
+from gops.sys_simulator.sys_run import PolicyRunner
+
+
+runner = PolicyRunner(
+    log_policy_dir_list=[
+        "../results/pyth_semitruck7dof/FHADP2_240222-142216"
+        # "PATH_TO_YOUR_RESULT_DIR",
+    ],
+    trained_policy_iteration_list=[
+        "91200_opt"
+        # "ITERATION_NUM",
+    ],
+    is_init_info=True,
+    init_info={
+        # parameters of env.reset()
+        "init_state": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 100, 94],
+    },
+    save_render=False,
+    legend_list=[
+        "FHADP2"
+        # "ALGORITHM_NAME",
+    ],
+    use_opt=False,
+    opt_args={
+        "opt_controller_type": "MPC",
+        "num_pred_step": 30,
+        "gamma": 1.0,
+        "mode": "collocation",
+        "minimize_options": {
+            "max_iter": 4000,
+            "tol": 1e-4,
+        },
+        "use_terminal_cost": False,
+        "use_MPC_for_general_env": True,
+    },
+    constrained_env=False,
+    is_tracking=True,
+    dt=0.01,
+)
+
+runner.run()
